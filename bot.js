b8b9037f75eda4c568bbad66d3f432d998e46f10
spamaltxzc <spammingaltacc@gmail.com>
2025-09-06 16:27:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,3 +79,10 @@ if (token === 'YOUR_BOT_TOKEN_HERE') {
 }
 
 client.login(token);
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is alive.'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Keep-alive server running on port ${PORT}`);
+});
